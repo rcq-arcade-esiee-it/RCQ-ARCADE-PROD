45dfe154daaa8898050d839db8ca836156765f7e
rcq-arcade-esiee-it <sajous.davenese@gmail.com>
2022-06-29 22:29:54 +0000

    See https://github.com/rcq-arcade-esiee-it/RCQ_Arcade/commit/7c36856a9e5915f5e6b7e299228b332cdc508b4a from refs/pull/16/merge

---
+++ Tests/TestsReferences.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class TestsReferences : MonoBehaviour
+{
+    #region CORE
+
+    public GameObject gameManagerPrefab;
+    public GameObject firstGameManagerPrefab;
+    public GameObject playerPrefab;
+    public GameObject mainCanvasPrefab;
+
+    #endregion
+
+    #region SCENES
+
+    public Object mainGameScene;
+    public Object gameMenuScene;
+    public Object firstGameScene;
+
+    #endregion
+
+    #region Collectibles
+
+    public GameObject rugbyBallPrefab;
+    public GameObject bombPrefab;
+
+    #endregion
+}
